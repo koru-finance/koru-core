0e63c706a9829c1b601b7f4579dafada6cb63e55
Joel Orzet <joelorzt@gmail.com>
2024-10-19 00:55:03 -0300

    feat: add transfer stablecoin token utility

--- contracts/user-contract/src/shared/utils.rs
+++ contracts/user-contract/src/shared/utils.rs
@@ -1,4 +1,6 @@
-use soroban_sdk::{BytesN, Env};
+use soroban_sdk::{token::TokenClient, Address, BytesN, Env};
+
+use crate::storage::storage;
 
 pub fn str_to_bytesn32(env: &Env, input: &str) -> BytesN<32> {
     let mut bytes_array = [0u8; 32];
@@ -8,4 +10,17 @@ pub fn str_to_bytesn32(env: &Env, input: &str) -> BytesN<32> {
     bytes_array[..len].copy_from_slice(&input_bytes[..len]);
 
     BytesN::from_array(env, &bytes_array)
+}
+
+// Divide by 100_000 to get 3 decimal places of accuracy when calculating percentages
+pub(crate) fn calculate_percentage(amount: u128, percentage: u128) -> u128 {
+    (amount * percentage) / 100_000
+}
+
+pub fn transfer_token(env: &Env, from: &Address, to: &Address, amount: u128) {
+    let token = storage::get_token(&env);
+
+    let client = TokenClient::new(&env, &token);
+
+    client.transfer(from, to, &(amount as i128));
 }
