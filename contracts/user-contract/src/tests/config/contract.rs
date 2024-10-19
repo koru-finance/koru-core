d5742296ed18a6a9ab6723633d85e4a8a205c5d5
Joel Orzet <joelorzt@gmail.com>
2024-10-19 00:57:17 -0300

    feat: add contract tests

--- contracts/user-contract/src/tests/config/contract.rs
+++ contracts/user-contract/src/tests/config/contract.rs
@@ -1,10 +1,23 @@
-use soroban_sdk::Env;
+use soroban_sdk::{
+    testutils::{Address as _, Ledger},
+    token::{StellarAssetClient, TokenClient},
+    Address, BytesN, Env,
+};
 
 use crate::contract::{UserContract, UserContractClient};
+
+use super::utils:: create_token;
 
 pub struct ContractTest<'a> {
     pub env: Env,
     pub contract: UserContractClient<'a>,
+    pub admin: Address,
+    pub deposit_address: Address,
+    pub protocol_address: Address,
+    pub user_a: Address,
+    pub bridge: Address,
+    pub token_client: TokenClient<'a>,
+    pub token_admin: StellarAssetClient<'a>,
 }
 
 impl<'a> ContractTest<'a> {
@@ -15,6 +28,51 @@ impl<'a> ContractTest<'a> {
         let contract_id = env.register_contract(None, UserContract {});
         let contract = UserContractClient::new(&env, &contract_id);
 
-        ContractTest { env, contract }
+        let admin = Address::generate(&env);
+        let deposit_address = Address::generate(&env);
+        let protocol_address = Address::generate(&env);
+        let user_a = Address::generate(&env);
+        let bridge = Address::generate(&env);
+
+        let (token_client, token_admin) = create_token(&env, admin.clone());
+
+        ContractTest {
+            env,
+            contract,
+            admin,
+            deposit_address,
+            protocol_address,
+            bridge,
+            user_a,
+            token_client,
+            token_admin,
+        }
+    }
+
+    pub fn initialize() -> Self {
+        let config = ContractTest::setup();
+
+        config.contract.initialize(
+            &config.admin.clone(),
+            &config.token_client.address.clone(),
+            &config.bridge.clone(),
+            &BytesN::from_array(&config.env, &[0; 32]),
+            &config.deposit_address.clone(),
+            &config.protocol_address.clone(),
+        );
+
+        config.contract.store_strategy(&1, &120, &12_000, &0);
+
+        let strategy = config.contract.get_strategy(&1);
+
+        assert_eq!(strategy.yield_rate, 12_000);
+
+        ContractTest { ..config }
+    }
+
+    pub fn add_ledger_time(env: &Env, time: u64) {
+        env.ledger().with_mut(|li| {
+            li.timestamp += time;
+        });
     }
 }
--- contracts/user-contract/src/tests/test.rs
+++ contracts/user-contract/src/tests/test.rs
@@ -1,16 +1,172 @@
-use crate::tests::config::contract::ContractTest;
-
-use soroban_sdk::{symbol_short, vec};
+use crate::{
+    shared::constants::SECONDS_PER_DAY,
+    tests::config::{contract::ContractTest, utils::add_decimals},
+};
 
 #[test]
-fn test() {
-    let config = ContractTest::setup();
+fn test_deposit() {
+    let config = ContractTest::initialize();
 
-    let ContractTest { env, contract } = config;
+    let ContractTest {
+        contract,
+        admin,
+        user_a,
+        token_client,
+        deposit_address,
+        ..
+    } = config;
 
-    let words = contract.hello(&symbol_short!("Dev"));
+    config
+        .token_admin
+        .mint(&user_a.clone(), &(add_decimals(10_000) as i128));
+
+    config
+        .token_admin
+        .mint(&contract.address.clone(), &(add_decimals(100_000) as i128));
+
+    assert_eq!(token_client.balance(&admin), 0);
+    assert_eq!(token_client.balance(&user_a), add_decimals(10_000) as i128);
+
+    let _result = contract.deposit(&user_a, &add_decimals(1000), &1);
+
     assert_eq!(
-        words,
-        vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),]
+        token_client.balance(&deposit_address),
+        add_decimals(1000) as i128
+    );
+
+    assert_eq!(token_client.balance(&user_a), add_decimals(9_120) as i128);
+
+    contract.set_investor_premium(&1000);
+    contract.set_protocol_premium(&1340);
+
+    contract.deposit(&user_a, &add_decimals(1000), &1);
+
+    assert_eq!(
+        token_client.balance(&deposit_address),
+        add_decimals(2000) as i128
+    );
+
+    assert_eq!(token_client.balance(&user_a), 8263_4000000 as i128);
+}
+
+#[test]
+#[should_panic = "Error(Contract, #4)"]
+fn test_should_panic_when_whitdraw() {
+    let config = ContractTest::initialize();
+
+    let ContractTest {
+        contract,
+        admin,
+        user_a,
+        token_client,
+        ..
+    } = config;
+
+    config
+        .token_admin
+        .mint(&user_a.clone(), &(add_decimals(10_000) as i128));
+
+    config
+        .token_admin
+        .mint(&contract.address.clone(), &(add_decimals(100_000) as i128));
+
+    assert_eq!(token_client.balance(&admin), 0);
+    assert_eq!(token_client.balance(&user_a), add_decimals(10_000) as i128);
+
+    let result = contract.deposit(&user_a, &add_decimals(1000), &1);
+
+    contract.withdraw(&user_a, &result.start_period, &result.end_period);
+}
+
+#[test]
+fn test_should_whitdraw_the_total_collateral() {
+    let config = ContractTest::initialize();
+
+    let ContractTest {
+        contract,
+        admin,
+        user_a,
+        token_client,
+        ..
+    } = config;
+
+    config
+        .token_admin
+        .mint(&user_a.clone(), &(add_decimals(10_000) as i128));
+
+    config
+        .token_admin
+        .mint(&contract.address.clone(), &(add_decimals(100_000) as i128));
+
+    assert_eq!(token_client.balance(&admin), 0);
+    assert_eq!(token_client.balance(&user_a), add_decimals(10_000) as i128);
+
+    let result = contract.deposit(&user_a, &add_decimals(1000), &1);
+    assert_eq!(token_client.balance(&user_a), add_decimals(9_120) as i128);
+
+    contract.update_withdraw_status(
+        &user_a,
+        &result.start_period.clone(),
+        &result.end_period.clone(),
+        &true,
+    );
+
+    ContractTest::add_ledger_time(&config.env, result.end_period.clone());
+    contract.withdraw(&user_a, &result.start_period, &result.end_period);
+
+    assert_eq!(
+        token_client.balance(&user_a),
+        add_decimals(10_000) as i128 + add_decimals(120) as i128
+    );
+}
+
+#[test]
+fn test_should_whitdraw_partial_collateral() {
+    let config = ContractTest::initialize();
+
+    let ContractTest {
+        contract,
+        admin,
+        user_a,
+        token_client,
+        ..
+    } = config;
+
+    config
+        .token_admin
+        .mint(&user_a.clone(), &(add_decimals(10_000) as i128));
+
+    config
+        .token_admin
+        .mint(&contract.address.clone(), &(add_decimals(100_000) as i128));
+
+    assert_eq!(token_client.balance(&admin), 0);
+    assert_eq!(token_client.balance(&user_a), add_decimals(10_000) as i128);
+
+    contract.set_investor_premium(&1000);
+    contract.set_protocol_premium(&1000);
+
+    let result = contract.deposit(&user_a, &add_decimals(1000), &1);
+    assert_eq!(token_client.balance(&user_a), add_decimals(9_140) as i128);
+
+    contract.set_koru_penalty(&1000);
+    contract.set_protocol_penalty(&1000);
+
+    contract.update_withdraw_status(
+        &user_a,
+        &result.start_period.clone(),
+        &result.end_period.clone(),
+        &true,
+    );
+
+    ContractTest::add_ledger_time(
+        &config.env,
+        result.end_period.clone() - SECONDS_PER_DAY * 30,
+    );
+    contract.withdraw(&user_a, &result.start_period, &result.end_period);
+
+    assert_eq!(
+        token_client.balance(&user_a),
+        add_decimals(10_000) as i128 + 1_349_460_000 as i128
     );
 }
